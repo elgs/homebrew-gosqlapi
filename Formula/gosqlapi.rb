e18ad495f466ba8f631d5b6ffd8c282d9571fcdf
Qian Chen <elgs1980@hotmail.com>
2025-09-08 17:49:06 -0700

    Update.

---
+++ Formula/gosqlapi.rb
@@ -0,0 +1,17 @@
+class Gosqlapi < Formula
+  desc "Turns any SQL database into a RESTful API."
+  homepage "https://github.com/elgs/gosqlapi"
+  url "https://github.com/elgs/gosqlapi.git", tag: "v0.1.0"
+  version "0.1.0"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "."
+  end
+
+  test do
+    system "#{bin}/gosqlapi", "--help"
+  end
+end
