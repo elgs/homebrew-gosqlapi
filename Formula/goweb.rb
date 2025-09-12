3335664ab73f0703fc7f4d6d081c905958d1aff6
Qian Chen <elgs1980@hotmail.com>
2025-09-11 23:45:29 -0700

    Update.

---
+++ Formula/goweb.rb
@@ -0,0 +1,12 @@
+class Goweb < Formula
+  desc "Multi domain/host web server written in Golang"
+  homepage "https://github.com/elgs/goweb"
+  url "https://github.com/elgs/goweb.git", tag: "v0.0.7"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "."
+  end
+end
